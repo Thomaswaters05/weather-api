3e74e0b2ede7a392189d15f1d6d1810fd6c4f94c
Tommy Waters <Thomas.Waters05@gmail.com>
2016-10-26 20:19:05 -0500

    finished first portion

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,22 +2,24 @@
 
 let apiKeys = {};
 
-let imageList = (searchText) => {
+let weatherList = (searchText) => {
   return new Promise ((resolve, reject) => {
     $.ajax({
       method: 'GET',
       url: 'apiKeys.json'
     }).then((response) => {
       apiKeys = response;
-      let authHeader = 'Client-ID ' + apiKeys.client_id;
+      let authHeader = apiKeys.key;
       $.ajax({
         method: 'GET',
-        headers:{
-          'Authorization': authHeader
-        },
-        url: ` https://api.imgur.com/3/gallery/t/${searchText}`
+        // headers:{
+        //   'Authorization': authHeader --> certain API's use this, while others do NOT
+        // },
+        url: `http://api.openweathermap.org/data/2.5/forecast/weather?zip=${searchText}&units=imperial&APPID=${authHeader}`
+
       }).then((response2)=>{
-        resolve(response2.data.items);
+        console.log(response2);
+        resolve(response2);
       }, (errorResponse2) => {
         reject(errorResponse2);
       });
@@ -33,14 +35,17 @@ $(document).ready(function() {
   $('#clicky-button').on('click', ()=>{
   $('#clicky-button').button('loading');
   $('output').html("");
-    let searchy = $('#imgur-search').val();
-    console.log("its working", searchy);
-    imageList(searchy).then((dataFromImgur)=>{
-      $('#clicky-button').button("reset");
-      console.log('dataFromImgur', dataFromImgur);
-      dataFromImgur.forEach((image) =>{
-      $('#output').append(`<img src="${image.link}">`);
-    });
+    let searchy = $('#weather-search').val();
+      console.log("its working", searchy);
+      weatherList(searchy).then((dataFromApi)=>{
+        $('#clicky-button').button("reset");
+        console.log("data",dataFromApi);
+        $('#output').append(`<div id="weather_info"> City: ${dataFromApi.city.name}</div>`);
+        $('#output').append(`<div id="weather_info"> Country: ${dataFromApi.city.country}</div>`);
+        $('#output').append(`<div id="weather_info"> Current Condition: ${dataFromApi.list[0].weather[0].description}</div>`);
+        $('#output').append(`<div id="weather_info"> Curent Wind Speed: ${dataFromApi.list[0].wind.speed}</div>`);
+        $('#output').append(`<div id="weather_info"> Curent Temperature: ${dataFromApi.list[0].main.temp}</div>`);
+
   });
  });
 });
